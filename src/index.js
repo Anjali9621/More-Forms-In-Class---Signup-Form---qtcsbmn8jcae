bd9676d673c996866c47904fcba7405808f805d3
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-02-02 14:12:37 +0530

    form

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
