684236e8b6b7ed3707a1960d40fa945e6e15b899
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-02-23 16:18:35 +0530

    str2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,7 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
 import React from "react";
 import "../styles/App.css";
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
   const st = {
     name: "",
     email: "",
